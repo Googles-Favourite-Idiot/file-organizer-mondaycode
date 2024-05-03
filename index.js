1bf0cdb0b073d3a8e40d4cf96795d0b1b9e1b04a
K Le <pydev420@protonmail.com>
2024-05-02 21:16:38 -0700

    update file transfer code

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-import express from "express";
-import { Logger } from "@mondaycom/apps-sdk";
-import { transformText } from "./src/transformation-service.js";
-import { authorizeRequest } from "./src/middleware.js";
-import { changeColumnValue, getColumnValue } from "./src/monday-api-service.js";
-import { getSecret, isDevelopmentEnv, getEnv } from "./src/helpers.js";
-import dotenv from "dotenv";
-import { readQueueMessage, produceMessage } from "./src/queue-service.js";
+import express from 'express';
+import { Logger } from '@mondaycom/apps-sdk';
+import { transformText } from './src/transformation-service.js';
+import { authorizeRequest } from './src/middleware.js';
+import { changeColumnValue, getColumnValue } from './src/monday-api-service.js';
+import { getSecret, isDevelopmentEnv, getEnv } from './src/helpers.js';
+import dotenv from 'dotenv';
+import { readQueueMessage, produceMessage } from './src/queue-service.js';
 dotenv.config();
 
-const logTag = "ExpressServer";
-const PORT = "PORT";
-const SERVICE_TAG_URL = "SERVICE_TAG_URL";
-const TO_UPPER_CASE = "TO_UPPER_CASE";
-const TO_LOWER_CASE = "TO_LOWER_CASE";
+const logTag = 'ExpressServer';
+const PORT = 'PORT';
+const SERVICE_TAG_URL = 'SERVICE_TAG_URL';
+const TO_UPPER_CASE = 'TO_UPPER_CASE';
+const TO_LOWER_CASE = 'TO_LOWER_CASE';
 
 const logger = new Logger(logTag);
 const currentPort = getSecret(PORT); // Port must be 8080 to work with monday code
@@ -21,15 +21,15 @@ const currentUrl = getSecret(SERVICE_TAG_URL);
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.status(200).send({ message: "healthy" });
+app.get('/', (req, res) => {
+  res.status(200).send({ message: 'healthy' });
 });
 
-app.post("/monday/execute_action", authorizeRequest, async (req, res) => {
+app.post('/monday/execute_action', authorizeRequest, async (req, res) => {
   logger.info(
     JSON.stringify({
-      message: "New request received",
-      path: "/action",
+      message: 'New request received',
+      path: '/action',
       body: req.body,
       headers: req.headers,
     })
@@ -47,76 +47,67 @@ app.post("/monday/execute_action", authorizeRequest, async (req, res) => {
       transformationType,
     } = inputFields;
 
-    const text = await getColumnValue(shortLivedToken, itemId, sourceColumnId);
-    if (!text) {
-      return res.status(200).send({});
-    }
-    const transformedText = transformText(
-      text,
-      transformationType ? transformationType.value : "TO_UPPER_CASE"
-    );
-
-    await changeColumnValue(
+    const fileName = await getFileColumnName(
       shortLivedToken,
-      boardId,
+      itemId,
+      sourceColumnId
+    );
+    const keyword = extractKeywordFromFileName(fileName);
+    const columnValue = keyword ? { files: [fileName] } : null;
+
+    await assignFileToColumn(
+      shortLivedToken,
       itemId,
       targetColumnId,
-      transformedText
+      keywordColumnId
     );
 
     return res.status(200).send({});
   } catch (err) {
     logger.error(err);
-    return res.status(500).send({ message: "internal server error" });
+    return res.status(500).send({ message: 'internal server error' });
   }
 });
 
 app.post(
-  "/monday/get_remote_list_options",
+  '/monday/get_remote_list_options',
   authorizeRequest,
   async (req, res) => {
     const TRANSFORMATION_TYPES = [
-      { title: "to upper case", value: TO_UPPER_CASE },
-      { title: "to lower case", value: TO_LOWER_CASE },
+      { title: 'to upper case', value: TO_UPPER_CASE },
+      { title: 'to lower case', value: TO_LOWER_CASE },
     ];
     try {
       return res.status(200).send(TRANSFORMATION_TYPES);
     } catch (err) {
       logger.error(err);
-      return res.status(500).send({ message: "internal server error" });
+      return res.status(500).send({ message: 'internal server error' });
     }
   }
 );
 
+app.post('/produce', async (req, res) => {
+  try {
+    const { body } = req;
+    const message = JSON.stringify(body);
+    const messageId = await produceMessage(message);
+    return res.status(200).send({ messageId });
+  } catch (err) {
+    logger.error(JSON.stringify(err));
+    return res.status(500).send({ message: 'internal server error' });
+  }
+});
 
-app.post(
-    "/produce",
-    async (req, res) => {
-        try {
-            const { body } = req;
-            const message = JSON.stringify(body);
-            const messageId = await produceMessage(message);
-            return res.status(200).send({ messageId });
-        } catch (err) {
-            logger.error(JSON.stringify(err));
-            return res.status(500).send({ message: "internal server error" });
-        }
-    }
-);
-
-app.post(
-    "/mndy-queue",
-    async (req, res) => {
-        try {
-            const { body, query } = req;
-            readQueueMessage({ body, query });
-            return res.status(200).send({}); // return 200 to ACK the queue message
-        } catch (err) {
-            logger.error(err.error);
-            return res.status(500).send({ message: "internal server error" });
-        }
-    }
-);
+app.post('/mndy-queue', async (req, res) => {
+  try {
+    const { body, query } = req;
+    readQueueMessage({ body, query });
+    return res.status(200).send({}); // return 200 to ACK the queue message
+  } catch (err) {
+    logger.error(err.error);
+    return res.status(500).send({ message: 'internal server error' });
+  }
+});
 
 app.listen(currentPort, () => {
   if (isDevelopmentEnv()) {
@@ -124,7 +115,7 @@ app.listen(currentPort, () => {
   } else {
     logger.info(
       `up and running listening on port:${currentPort}`,
-      "server_runner",
+      'server_runner',
       {
         env: getEnv(),
         port: currentPort,
--- src/monday-api-service.js
+++ src/monday-api-service.js
@@ -5,3 +5,56 @@ const logTag = 'Middleware';
 const logger = new Logger(logTag);
 
 // insert new code here
+export const getFileColumnName = async (token, itemId, columnId) => {
+  try {
+    const mondayClient = initMondayClient();
+    mondayClient.setApiVersion('2024-01');
+    mondayClient.setToken(token);
+
+    const query = `query($itemId: [ID!], $columnId: [String!]) {
+      items (ids: $itemId) {
+        column_values(ids:$columnId) {
+          value
+        }
+      }
+    }`;
+    const variables = { columnId, itemId };
+
+    const response = await mondayClient.api(query, { variables });
+    return response.data.items[0].column_values[0].value;
+  } catch (err) {
+    logger.error(err);
+  }
+};
+
+export const assignFileToColumn = async (
+  token,
+  itemId,
+  fileColumnId,
+  keywordColumnId
+) => {
+  try {
+    const fileName = await getFileColumnName(token, itemId, fileColumnId);
+    const keyword = extractKeywordFromFileName(fileName);
+    const columnValue = keyword ? { files: [fileName] } : null;
+
+    const mondayClient = initMondayClient();
+    mondayClient.setApiVersion('2024-01');
+    mondayClient.setToken(token);
+
+    const query = `mutation($itemId: Int!, $columnId: String!, $value: JSON!) {
+      change_column_value(item_id: $itemId, column_id: $columnId, value: $value) {
+        id
+      }
+    }`;
+    const variables = { itemId, columnId: keywordColumnId, value: columnValue };
+
+    await mondayClient.api(query, { variables });
+  } catch (err) {
+    logger.error(err);
+  }
+};
+
+const extractKeywordFromFileName = (fileName) => {
+  // Logic to extract keyword from file name
+};
